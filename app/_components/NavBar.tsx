8d87366813ca49dc447e96854054c41cb9a431e8
Talibé BALDE <balde.talibe93@gmail.com>
2023-09-27 18:51:04 +0200

    ajout du composant about me

--- app/_components/NavBar.tsx
+++ app/_components/NavBar.tsx
@@ -5,31 +5,37 @@ import Link from "next/link"
 
 const NavBar = () => {
     return(
-        <nav className="flex justify-between items-center p-4 border border-slate-100 bg-white/50 backdrop-blur-lg drop-shadow-sm rounded-2xl">
-            <Link href={"/"} className="text-2xl font-semibold flex flex-row justify-between">
+        <nav className="flex justify-between items-center p-4 border border-slate-100 bg-white/30 backdrop-blur-lg drop-shadow-sm rounded-2xl">
+            <Link href={"/"} className="text-3xl font-semibold flex flex-row justify-between">
                 <Layers size={32}/>
-                <span className="pl-3">Libétech</span>
+                <span className="pl-3">LibéTech</span>
             </Link>
             <NavigationMenu>
                 <NavigationMenuList className="gap-3">
                     <NavigationMenuItem>
                         <Link href="/docs" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle({
+                                className: "bg-transparent hover:bg-white"
+                            })}>
                                 Who is Talibé ?
                             </NavigationMenuLink>
                         </Link>
                     </NavigationMenuItem>
                     <NavigationMenuItem>
                         <Link href="/docs" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                projects ?
+                            <NavigationMenuLink className={navigationMenuTriggerStyle({
+                                className: "bg-transparent hover:bg-white"
+                            })}>
+                                Projects
                             </NavigationMenuLink>
                         </Link>
                     </NavigationMenuItem>
                     <NavigationMenuItem>
                         <Link href="/docs" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                work with me
+                            <NavigationMenuLink className={navigationMenuTriggerStyle({
+                                className: "bg-primary text-primary-foreground hover:bg-primary/90 hover:text-fuchsia-50"
+                            })}>
+                                Work with me
                             </NavigationMenuLink>
                         </Link>
                     </NavigationMenuItem>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,33 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import NavBar from './_components/NavBar'
+import { Download, MessageSquare } from 'lucide-react'
+import AboutMe from './_components/AboutMe'
 
 export default function Home() {
   return (
-    <div className="p-8 bg-gradient-to-tr from-indigo-500 from-0% via-sky-500 via-30% to-emerald-700 to-100% min-h-screen">
-      <NavBar></NavBar>
+    <div className="pt-6 bg-gradient-to-tr from-indigo-500 from-0% via-sky-500 via-30% to-emerald-700 to-100% min-h-screen">
+      <div className='px-8'>
+        <NavBar/>
+      </div>
+      <div className='container pl-40'>
+        <div className='pt-12 flex flex-col gap-3 mb-8'>
+          <p className='text-2xl font-semibold'>Hello there ! 👋</p>
+          <h1 className='text-4xl font-bold'>I&apos;m Talibé BALDE</h1>
+          <h2 className='text-3xl font-semibold'> a passionate web developer</h2>
+        </div>
+        <div className='flex gap-4 mb-12'>
+          <Button className='gap-2' type='button'>
+            <Download size={16}/>
+            Download my resume
+          </Button>
+          <Button className='gap-2' type='button' variant={'secondary'}>
+            <MessageSquare size={16}/>
+            Text me
+          </Button>
+        </div>
+        <AboutMe/>
+      </div>
     </div>
   )
 }
