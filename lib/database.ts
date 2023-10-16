161cb0bafcfba985ce9f1068d36fee53be3cba79
Talibé BALDE <balde.talibe93@gmail.com>
2023-10-16 10:46:59 +0200

    correction bug deploy

--- lib/database.ts
+++ lib/database.ts
@@ -2,4 +2,4 @@ import {createClient } from '@supabase/supabase-js'
 
 import { Env } from '@/utils/env.util'
 
-export const supabase = createClient(Env.SUPABASE_URL, Env.SUPABASE_KEY)
+export const supabase = createClient(Env.NEXT_PUBLIC_SUPABASE_URL, Env.NEXT_PUBLIC_SUPABASE_KEY)
--- utils/env.util.ts
+++ utils/env.util.ts
@@ -1,12 +1,12 @@
 import {z} from "zod"
 
 const EnvSchema = z.object({
-    SUPABASE_URL: z.string().min(1, 'Missing SUPABASE_URL').url('Invalid SUPABASE_URL'),
-    SUPABASE_KEY: z.string().min(1, 'Missing SUPABASE_KEY'),
+    NEXT_PUBLIC_SUPABASE_URL: z.string().min(1, 'Missing SUPABASE_URL').url('Invalid SUPABASE_URL'),
+    NEXT_PUBLIC_SUPABASE_KEY: z.string().min(1, 'Missing SUPABASE_KEY'),
 })
 
 export const Env = EnvSchema.parse({
     ...process.env,
-    SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
-    SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY ?? '',
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+    NEXT_PUBLIC_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY ?? '',
 })
